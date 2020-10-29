3de82327b9f54b269dd07c9a096d2ff8e6a437fc
Gabriel Henton <gabehenton@gmail.com>
2020-10-28 19:50:37 -0700

    fixed the line-through completed items behavior

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,13 +2,13 @@ import React from "react";
 //import './components/Todo.css'
 
 const Todo = (props) => {
-	const complete = (evt) => {
+	const complete = (e) => {
 		props.completeItem(props.list.id);
 	};
 
 	return  <div 
 			  onClick={complete}
-			  
+			  className={`item${props.list.completed ? " completed" : ""}`}
 			>
 			  {props.list.task}
 			  
